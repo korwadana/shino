29c124c05f44d80a983bc9a0d204ca38fff3dc9d
korwadana <saadsalim@gmail.com>
2014-10-25 16:28:02 +0500

    uploaded laravel api test build

---
+++ public/js/lib/chart/templates.js
@@ -0,0 +1,13 @@
+/*
+  Repo: https://github.com/chinmaymk/angular-charts
+  LICENSE: https://github.com/chinmaymk/angular-charts/blob/master/LICENSE
+ */
+angular.module('angularChartsTemplates', ['left']);
+
+angular.module("left", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("left",
+    "<div class='ac-legend' style='float:left; witdh:25%'>\n" +
+    "</div>\n" +
+    "<div class='ac-chart' style='float:left; witdh:75%'>\n" +
+    "</div>");
+}]);
